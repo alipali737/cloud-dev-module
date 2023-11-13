823a2429294dfd4cd0e10ce284ef23f056e8bde1
alipali737 <alipali737@gmail.com>
2023-11-13 14:39:16 +0000

    Testing jQuery Post

--- resources/js/index.js
+++ resources/js/index.js
@@ -3,46 +3,16 @@ const invokeAPI = "https://02z3rs3dlg.execute-api.eu-west-2.amazonaws.com/prod"
 $(document).ready(function () {
     $("#pets-form").submit(function (event) {
         event.preventDefault();
-
-        var formData = {};
-        $(this).serializeArray().forEach(function(item) {
-            formData[item.name] = item.value;
-        });
-
-        $.ajax({
-            url: invokeAPI+"/pets",
-            method: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(formData),
-            success: function (data) {
+        $.post(invokeAPI + '/pets', $(this).serialize(), function (data) {
                 console.log("Success: ", data);
                 location.reload();
-            },
-            error: function (error) {
-                console.error("Error: ", error);
-            }
-        });
+            });
     });
     $("#walkers-form").submit(function (event) {
         event.preventDefault();
-
-        var formData = {};
-        $(this).serializeArray().forEach(function(item) {
-            formData[item.name] = item.value;
-        });
-
-        $.ajax({
-            url: invokeAPI+"/walkers",
-            method: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(formData),
-            success: function (data) {
-                console.log("Success: ", data);
-                location.reload();
-            },
-            error: function (error) {
-                console.error("Error: ", error);
-            }
+        $.post(invokeAPI + '/walkers', $(this).serialize(), function (data) {
+            console.log("Success: ", data);
+            location.reload();
         });
     });
 });
