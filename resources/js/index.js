0b655b77330f103d4126a0b042eac346ff52ed72
alipali737 <alipali737@gmail.com>
2023-11-13 15:56:49 +0000

    Fixed POST requests on registration

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,18 +1,63 @@
 const invokeAPI = "https://02z3rs3dlg.execute-api.eu-west-2.amazonaws.com/prod"
 
-$(document).ready(function () {
-    $("#pets-form").submit(function (event) {
-        event.preventDefault();
-        $.post(invokeAPI + '/pets', $(this).serialize(), function (data) {
-                console.log("Success: ", data);
-                location.reload();
-            });
+const walkersForm = document.getElementById("walkers-form");
+walkersForm.addEventListener('submit', submitWalkerForm);
+
+async function submitWalkerForm(event) {
+    event.preventDefault();
+    var formData = {};
+    var formElements = event.target.elements;
+    for (let i = 0; i < formElements.length; i++) {
+        const element = formElements[i];
+        if (element.name) {
+            formData[element.name] = element.value;
+        }
+    }
+
+    var jsonData = JSON.stringify(formData);
+
+    console.log("JSON: " + jsonData);
+
+    const response = await fetch(invokeAPI+"/walkers", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: jsonData,
     });
-    $("#walkers-form").submit(function (event) {
-        event.preventDefault();
-        $.post(invokeAPI + '/walkers', $(this).serialize(), function (data) {
-            console.log("Success: ", data);
-            location.reload();
-        });
+    
+    response.json().then((data) => {
+        console.log(data);
     });
-});
+}
+
+const petsForm = document.getElementById("pets-form");
+petsForm.addEventListener('submit', submitWalkerForm);
+
+async function submitWalkerForm(event) {
+    event.preventDefault();
+    var formData = {};
+    var formElements = event.target.elements;
+    for (let i = 0; i < formElements.length; i++) {
+        const element = formElements[i];
+        if (element.name) {
+            formData[element.name] = element.value;
+        }
+    }
+
+    var jsonData = JSON.stringify(formData);
+
+    console.log("JSON: " + jsonData);
+
+    const response = await fetch(invokeAPI+"/pets", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: jsonData,
+    });
+    
+    response.json().then((data) => {
+        console.log(data);
+    });
+}
