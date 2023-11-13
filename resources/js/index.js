ba209dfa657530bbdcb03204a048c9e112f06aac
alipali737 <alipali737@gmail.com>
2023-11-13 10:19:40 +0000

    Changed POST requests to use jQuery

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,40 +1,48 @@
 const invokeAPI = "https://02z3rs3dlg.execute-api.eu-west-2.amazonaws.com/prod"
 
-document.addEventListener('DOMContentLoaded', () => {
-    var petsForm = document.getElementById("pets-form");
-    petsForm.addEventListener('submit', (event) => {
+$(document).ready(function () {
+    $("#pets-form").submit(function (event) {
         event.preventDefault();
-        handleFormSubmission(invokeAPI+"/pets", petsForm);
-    });
 
-    var walkersForm = document.getElementById("walkers-form");
-    walkersForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        handleFormSubmission(invokeAPI+"/walkers", walkersForm);
-    });
-
-    function handleFormSubmission(endpoint, form) {
-        var formData = new FormData(form);
-
-        fetch(endpoint, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Error adding details: Request failed with status ' + response.status);
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Success: ', data);
-            location.reload();
-        })
-        .catch(error => {
-            console.error('Error: ', error);
+        var formData = {};
+        $(this).serializeArray().forEach(function(item) {
+            formData[item.name] = item.value;
         });
-    }
+
+        $.ajax({
+            url: invokeAPI+"/pets",
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(formData),
+            success: function (data) {
+                console.log("Success: ", data);
+                location.reload();
+            },
+            error: function (error) {
+                console.error("Error: ", error);
+            }
+        });
+    });
+    $("#walkers-form").submit(function (event) {
+        event.preventDefault();
+
+        var formData = {};
+        $(this).serializeArray().forEach(function(item) {
+            formData[item.name] = item.value;
+        });
+
+        $.ajax({
+            url: invokeAPI+"/walkers",
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(formData),
+            success: function (data) {
+                console.log("Success: ", data);
+                location.reload();
+            },
+            error: function (error) {
+                console.error("Error: ", error);
+            }
+        });
+    });
 });
